0d906f9cc7f5ff2fdaddd58b925b5f724b6a707c
Ariesta Agung <ariesta.work@gmail.com>
2022-12-22 17:01:15 +0700

    Initial Commit

---
+++ TheMovieCatalogue/TheMovieCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieCatalogueApp.swift
+//  TheMovieCatalogue
+//
+//  Created by BRIMO on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
